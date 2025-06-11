e0e64657b1c5e88edf6cb6de41fb5d7977fe2ca0
huzeyfeyer <huzeyfeyer1@gmail.com>
2025-06-11 09:53:18 +0200

    Initial Spring Boot project

---
+++ src/main/java/be/ehb/weer_applicatie/WeerApplicatieApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.weer_applicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeerApplicatieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeerApplicatieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/weer_applicatie/WeerApplicatieApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.weer_applicatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeerApplicatieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
